0efde2baa2925cb7a9472a1b53dbfebee7bacb39
RedMatOnyx <art17004@byui.edu>
2020-10-24 23:09:01 -0700

    preston-4 site UNDER CONSTRUCTION, nav bar was going horiz fine, but now it wraps...

---
+++ lesson4/js/hamburgermenu.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    // console.log("it worked...I'm giddy with excitement!");
+    // console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
