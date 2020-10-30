98b4f88b670a7e4c941d087c8f4905b29b4fbd0f
RedMatOnyx <art17004@byui.edu>
2020-10-30 15:21:06 -0700

    TA push ham menu issues

--- lesson4/js/hamburgermenu.js
+++ lesson4/js/hamburgermenu.js
@@ -1,3 +1,5 @@
+
+// NOT USING THIS File, INSTEAD USE THE ONE WITH QUERY Selection, NO BUTTON
 function toggleMenu() {
     // console.log("it worked...I'm giddy with excitement!");
     // console.log(document.getElementById("primaryNav").classList);
