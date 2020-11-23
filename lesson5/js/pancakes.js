456d6b5f1ae76d0121c825dbb3edb0e257879e96
RedMatOnyx <art17004@byui.edu>
2020-11-22 18:17:10 -0800

    display pancake message on friday for saturday

---
+++ lesson5/js/pancakes.js
@@ -0,0 +1,6 @@
+let day = new Date().getDay();
+if (day == 0) {
+    document.querySelector(".message").style.backgroundColor = "pink";
+    document.querySelector(".message").style.visibility = "visible";
+    }
+
