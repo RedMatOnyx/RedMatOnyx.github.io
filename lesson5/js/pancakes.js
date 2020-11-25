db1e666061e8078e83d83b547c25b64d917a8f99
RedMatOnyx <art17004@byui.edu>
2020-11-24 20:36:07 -0800

    active state locked for preston menu button, etc.

--- lesson5/js/pancakes.js
+++ lesson5/js/pancakes.js
@@ -1,6 +1,7 @@
 let day = new Date().getDay();
-if (day == 6) {
+if (day == 0) {
     document.querySelector(".message").style.backgroundColor = "pink";
     document.querySelector(".message").style.visibility = "visible";
     }
-
+// To change the display property in JavaScript, consider the use of .style.display = "block" in a selection structure 
+// where the condition looks at the day of the week through the Date() object and getDay() method.
