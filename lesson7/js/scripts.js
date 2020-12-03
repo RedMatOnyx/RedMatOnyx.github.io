9a1e6b7c3f752a4012434aee0bff5576fa318cef
RedMatOnyx <art17004@byui.edu>
2020-12-02 16:17:41 -0800

    lazyload page with css and js ready to submit

--- lesson7/js/scripts.js
+++ lesson7/js/scripts.js
@@ -72,44 +72,9 @@ document.getElementById("chill").innerHTML = `${windchill}`;
 // ::::::::::::CONTACT SYMBOLS :::::::::::::::::::::::::
 src='https://kit.fontawesome.com/a076d05399.js'
 
-// :::::::::::::PROGRESSIVE LAZY LOAD IMAGES :::::::::::::::::
-// ok
-const images = document.querySelectorAll('[data-src]');
+// :::::::::::::PROGRESSIVE LAZY LOAD GALLERY IMAGES :::::::::::::::::
+/*
+img effects for gallery page
+.norm:hover {box-shadow: 0 0 50px #333;}
 
-    // ok
-    function preloadImage(img) {
-        const src = img.getAttribute("data-src");
-        if(!src) {
-            return;
-        }
-        img.src = src;
-        img.removeAttribute("data-src") // testing
-    }    
-        // const loadImages = (image) => {
-    //     image.setAttribute('src', image.getAttribute('data-src'));
-    //     image.onload = () => {image.removeAttribute('data-src');
-    //     };
-    // };
-const imgOptions = {
-threshold: 1,
-rootMargin: "0px 0px 50px 0px"
-};
-// if('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items, imgObserver) => {
-    items.forEach(item => {
-        if(!item.isIntersecting) {
-            return;
-        } else {
-            preloadImage(item.target);
-            imgObserver.unobserve(item.target);        
-        }
-    })
-}, imgOptions);
-
-images.forEach(image => {
-    imgObserver.observe(image);
-});
-
-imagesToLoad.forEach((img) => {
-        loadImages(img);
-});
+*/
