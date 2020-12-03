631e01d8263b05468c2df41014b8493e8fce0c3d
RedMatOnyx <art17004@byui.edu>
2020-12-02 23:08:14 -0800

    gallery looks good, but tries to find windchill not on page, need to seperate out all .js files and only reference as needed

--- lesson7/js/scripts.js
+++ lesson7/js/scripts.js
@@ -73,32 +73,4 @@ document.getElementById("chill").innerHTML = `${windchill}`;
 src='https://kit.fontawesome.com/a076d05399.js'
 
 // :::::::::::::PROGRESSIVE LAZY LOAD GALLERY IMAGES :::::::::::::::::
-const imagesToLoad = document.querySelectorAll("img[data-src]");
-
-const imgOptions = {
-    threshold: .5,
-    rootMargin: "0px 0px -50px 0px"
-};
-
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {image.removeAttribute('data-src');};
-};
-if('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver ((items, imgObserver) => {
-        items.forEach((item) => {
-            if(item.isIntersecting) {
-                loadImages(item.target);
-                imgObserver.unobserve(item.target);
-            }
-        });
-    }, imgOptions);
-
-        imagesToLoad.forEach((img) => {
-            imgObserver.observe(img);
-        });
-} else{
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-});
-}
+// found in lazy.js
