c7ea62c74da5b795b4c93b78213e01f8d126306b
RedMatOnyx <art17004@byui.edu>
2020-12-10 15:01:36 -0800

    JSON prophets page complete lesson9

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -11,29 +11,27 @@ fetch(requestURL)
     prophets.forEach(prophet => {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
     let image = document.createElement('img');
     // h2.textContent = prophets[i].name + ' ' + prophets[i].lastname; / use string interpolation instead, below
 
     //add details and attributes
     // h2.innerHTML = `${prophets[i].name} <span class="highlight">${prophets[i].lastname}</span> - Children: ${prophets[i].numofchildren}`;
-    h2.innerHTML = `${prophet.name} <span class="highlight">${prophet.lastname}</span> - Children: ${prophet.numofchildren}`;
+    h2.innerHTML = `${prophet.name} <span class="highlight">${prophet.lastname}`; // </span> - Children: ${prophet.numofchildren}`;
+    p1.innerHTML = `Date of Birth: ${prophet.birthdate}`;
+    p2.innerHTML = `Place of Birth: ${prophet.birthplace}`;
     // image.setAttribute('src', prophets[i].imageurl);
     image.setAttribute('src', prophet.imageurl);
     // image.setAttribute('alt', `Official Portrait of ${prophets[i].name} ${prophets[i].lastname}`);
-    image.setAttribute('alt', `Official Portrait of ${prophet.name} ${prophet .lastname}`);
+    image.setAttribute('alt', `Official Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}`);
 // actually add the content to the container
     card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
     card.appendChild(image);
 
     document.querySelector('div.cards').appendChild(card);
-
-    // birth date
-    
-    // birth place
-
-    // img
-    // 
-
 
     }); //added parenthesis and semi-colon per refactoring function above
 
