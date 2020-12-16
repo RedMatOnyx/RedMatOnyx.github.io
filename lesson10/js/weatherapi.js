55aaabd70fc849137c5108b221e9788bdd7a3060
RedMatOnyx <art17004@byui.edu>
2020-12-15 19:55:25 -0800

    br preston weatherapi (API activity done, but json api not applied to preston yet)

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,15 +1,21 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=9d82d746ab9b4a97a81f74aae8a3582b";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=9d82d746ab9b4a97a81f74aae8a3582b";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {console.log(jsObject);
 
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', 'icon matching weather description');
-// const tempK = jsObject.main.temp;
-// const tempF = (kelvintemp - 273.15)*9/5+32;
-    document.getElementById('current-temp').textContent = Math.round((jsObject.main.temp- 273.15)*9/5+32);
+    const curtemp = document.querySelector('#current-temp');
+    const iconsource = document.querySelector('#imagesrc');
+    const weathericon = document.querySelector('#icon');
+
+    curtemp.innerHTML = jsObject.main.temp;
+
+    iconsource.textContent = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+
+    // const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+    //rather do string interpolation below instead
+    const isrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`
+    
+    weathericon.setAttribute('src', isrc);  // focus on setAttribute()
+    weathericon.setAttribute('alt', jsObject.weather[0].description);
 
   });
