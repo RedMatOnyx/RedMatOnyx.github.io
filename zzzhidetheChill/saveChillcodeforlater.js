aa7231f5abb00d0ce4e933baa42949abcf47d69b
RedMatOnyx <art17004@byui.edu>
2020-12-16 22:15:34 -0800

    submitting as is lesson 11 weather site data is good but images and content not unique.

--- zzzhidetheChill/saveChillcodeforlater.js
+++
@@ -1,6 +0,0 @@
-// pulled from lesson 7
-
-let t = document.getElementById("currTemp").innerHTML;
-let s = document.getElementById("speed").innerHTML;
-let windchill = Math.round(35.74 + 0.6215 * t - 35.75 * s**0.16 + 0.4275 * t * s**0.16);
-document.getElementById("chill").innerHTML = `${windchill}`;
